00e2e4a2633b0e661d6861faf1cb15be09ba8fc9
Ryan Cloherty <cloherty.ryan@gmail.com>
2020-12-18 18:15:56 -0500

    Fixed code arrangement
    THAT MAKES SO MUCH MORE SENSE OH MY GOD

--- test/utils/Config.test.ts
+++ test/utils/Config.test.ts
@@ -5,47 +5,47 @@ import {stringify} from "ini";
 import {existsSync, readFileSync} from "fs";
 import {Config} from "../../src/utils/Config";
 
-const config_directory: string = join(__dirname, "..", "..");
-let config: { [p: string]: any } = {
-    steam: {
-        steam_web_api_key: 'XXXXXXXXXXXXXXXXXXXXXXX',
-        steamID64: '76561197960435530',
-    }, csgo: {
-        netcon_port: 2121,
-        csgo_demos_folder: 'C:\\Program Files (x86)\\Steam\\steamapps\\common\\Counter-Strike Global Offensive\\csgo',
-    }, demo_recording_helper: {
-        record_my_voice_in_demos: 1,
-        mute_my_voice_while_recording: 1,
-    }, demo_playback_helper: {
-        playback_voice_player_volume: 1,
-    }, demo_naming_helper: {
-        explicitly_mark_competitive_demos: 0,
-        attempt_hide_map_prefix: 1,
-    }, internals: {
-        log_level: 'info',
-        console_output_promise_wait_time: 2,
-        console_user_input_wait_time: 30,
-    }
-}
-
-beforeEach(function () {
-    mock({
-        get [join(config_directory, "config.ini")]() {
-            return stringify(config);
-        },
-        get [join(config_directory, "config.template.ini")]() {
-            return stringify(config);
-        },
-        'C:/Program Files (x86)/Steam/steamapps/common/Counter-Strike Global Offensive': {
-            //csgo.exe just needs to exist. We don't read the content so it's fine to just have placeholder content
-            'csgo.exe': Buffer.from([8, 6, 7, 5, 3, 0, 9]),
-        },
-    });
-})
-
-afterEach(mock.restore)
-
 describe("Config", function () {
+    const config_directory: string = join(__dirname, "..", "..");
+    let config: { [p: string]: any } = {
+        steam: {
+            steam_web_api_key: 'XXXXXXXXXXXXXXXXXXXXXXX',
+            steamID64: '76561197960435530',
+        }, csgo: {
+            netcon_port: 2121,
+            csgo_demos_folder: 'C:\\Program Files (x86)\\Steam\\steamapps\\common\\Counter-Strike Global Offensive\\csgo',
+        }, demo_recording_helper: {
+            record_my_voice_in_demos: 1,
+            mute_my_voice_while_recording: 1,
+        }, demo_playback_helper: {
+            playback_voice_player_volume: 1,
+        }, demo_naming_helper: {
+            explicitly_mark_competitive_demos: 0,
+            attempt_hide_map_prefix: 1,
+        }, internals: {
+            log_level: 'info',
+            console_output_promise_wait_time: 2,
+            console_user_input_wait_time: 30,
+        }
+    }
+
+    beforeEach(function () {
+        mock({
+            get [join(config_directory, "config.ini")]() {
+                return stringify(config);
+            },
+            get [join(config_directory, "config.template.ini")]() {
+                return stringify(config);
+            },
+            'C:/Program Files (x86)/Steam/steamapps/common/Counter-Strike Global Offensive': {
+                //csgo.exe just needs to exist. We don't read the content so it's fine to just have placeholder content
+                'csgo.exe': Buffer.from([8, 6, 7, 5, 3, 0, 9]),
+            },
+        });
+    })
+
+    afterEach(mock.restore);
+
     it("should find config.ini", function () {
         //From the docs at https://www.chaijs.com/api/bdd/#throwerrorlike-errmsgmatcher-msg
         //"... when testing if a function named fn throws, provide fn instead of fn() as the target for the assertion."
