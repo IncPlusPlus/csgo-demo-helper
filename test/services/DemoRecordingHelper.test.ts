c4e06a54fe0e73269bfa79a744e380340efc8887
Ryan Cloherty <cloherty.ryan@gmail.com>
2021-01-08 17:38:20 -0500

    Greatly simplified the big test in DemoRecordingHelper.test.ts + added more

--- test/services/DemoRecordingHelper.test.ts
+++ test/services/DemoRecordingHelper.test.ts
@@ -9,6 +9,11 @@ import {SubscriberManager} from '../../src/utils/SubscriberManager';
 import {DemoRecordingHelper} from "../../src/services/DemoRecordingHelper";
 import {VoicePlayerVolume} from "../../src/utils/VoicePlayerVolume";
 import {DemoNamingHelper} from "../../src/utils/DemoNamingHelper";
+import * as mock from "mock-fs";
+import {join} from 'path';
+import {Pair} from "../../src/utils/Pair";
+import {Cvars} from "../../src/utils/Cvars";
+import {ConsoleHelper} from "../../src/utils/ConsoleHelper";
 import _ = require("mitm");
 
 describe("DemoRecordingHelper", function () {
@@ -39,8 +44,7 @@ describe("DemoRecordingHelper", function () {
             console_user_input_wait_time: 30,
         }
     };
-    const expectedCommandsWhenRecordCommandIssued = ['game_mode\n', 'game_type\n', 'status\n', 'echo\n', 'echo\n', 'echo\n', 'echo\n', 'echo\n', 'voice_loopback 1\n'];
-    const expectedCommandsWhenRecordingStartsSuccessfully = ['echo\n', 'echo\n', 'echo\n', 'echo\n', 'echo\n', 'echo DemoHelper started recording demo successfully!\n', 'echo\n', 'echo\n', 'echo\n', 'echo\n', 'echo\n', 'echo DemoHelper applied recording preferences and recorded a message in demo successfully!\n', 'echo DemoHelper set the volume of player The Lovely Potato to 0.\n'];
+    const expectedCommandsWhenRecordingStartsSuccessfully = ['echo DemoHelper started recording demo successfully!\n', 'echo DemoHelper applied recording preferences and recorded a message in demo successfully!\n', 'echo DemoHelper set the volume of player The Lovely Potato to 0.\n'];
     const statusLineValveMatchmakingServer = (mapName: string) => {
         return `Connected to =[A:1:1075843077:16207]:0
 hostname: Valve CS:GO US East Server (srcds084.121.5)
@@ -109,69 +113,176 @@ netcon  :  172.30.160.1:2121
         sandbox.restore();
     });
 
-    describe("The basics", function () {
-        const sandbox = createSandbox();
-        const expectedResponses = expectedCommandsWhenRecordCommandIssued.concat('record casual-office-1-1-2021\n');
-        const commandResponseObject: { [key: string]: string } = {
-            game_mode: `"game_mode" = "0"`,
-            game_type: `"game_type" = "0"`,
-            status: `${statusLineValveMatchmakingServer('cs_office')}`,
-            'record casual-office-1-1-2021': `Recording to casual-office-1-1-2021.dem...`,
-        };
+    describe("Recording scenarios", function () {
 
-        afterEach(function () {
-            sandbox.restore();
-        });
+        // This suite is for the if/else block in attemptStartRecording
+        describe("Handling the different outcomes of the record command", function () {
+            it(`All defaults. New demo and no others exist at the moment. Demo recording begins successfully`, async function () {
+                //Uncomment this line to get logger output during this test
+                // LogHelper.configure(config);
 
-        it(`All defaults. New demo and no others exist at the moment. Demo recording begins successfully`, async function () {
-            let recording = false;
-            let preRecordingIndex = 0;
-            let postRecordingIndex = 0;
-            mitm.on("connection", function (s) {
-                s.on("data", function (data) {
-                    if (!recording) {
-                        expect(data.toString()).eq(expectedResponses[preRecordingIndex]);
-                        if (!(data.toString().startsWith('echo') || data.toString().startsWith('voice_loopback'))) {
-                            s.write(`${commandResponseObject[data.toString().trim()]}\n`);
-                        }
-                        preRecordingIndex++;
-                        if (preRecordingIndex === expectedResponses.length) {
-                            recording = true;
-                        } else {
+                const expectedRecordCommand = 'record casual-office-1-1-2021\n';
+                let recording = false;
+                let postRecordingIndex = 0;
+                mitm.on("connection", function (s) {
+                    s.on("data", function (data) {
+                        if (!recording) {
+                            expect(data.toString()).eq(expectedRecordCommand);
                             expect(DemoRecordingHelper.synchronouslyCheckIfRecording()).eq(false);
-                        }
-                    } else {
-                        expect(data.toString()).eq(expectedCommandsWhenRecordingStartsSuccessfully[postRecordingIndex]);
-                        if (!(data.toString().startsWith('echo') || data.toString().startsWith('voice_loopback'))) {
-                            s.write(`${commandResponseObject[data.toString().trim()]}\n`);
-                        }
-                        postRecordingIndex++;
-                        if (postRecordingIndex >= expectedCommandsWhenRecordingStartsSuccessfully.length) {
-                            s.write('Completed demo, recording time 45.7, game frames 1807.\n');
-                            s.end();
+                            if (data.toString() === expectedRecordCommand) {
+                                s.write('Recording to casual-office-1-1-2021.dem...\n');
+                                recording = true;
+                            }
                         } else {
                             expect(DemoRecordingHelper.synchronouslyCheckIfRecording()).eq(true);
+                            expect(data.toString()).eq(expectedCommandsWhenRecordingStartsSuccessfully[postRecordingIndex]);
+                            postRecordingIndex++;
+                            if (postRecordingIndex >= expectedCommandsWhenRecordingStartsSuccessfully.length) {
+                                s.write('Completed demo, recording time 45.7, game frames 1807.\n');
+                                s.end();
+                            }
                         }
+                    });
+                    s.write(`${DemoRecordingHelper.BeginRecordingCommand}\n`);
+                });
+                const setCvarStub = sandbox.stub(Cvars, "setCvar");
+                const getCvarStub = sandbox.stub(Cvars, "getCvar");
+                const getMapNameStub = sandbox.stub(DemoNamingHelper, "getMapName");
+                sandbox.stub(ConsoleHelper, "padConsole");
+                getCvarStub.withArgs('game_mode').returns(new Promise(resolve => resolve(0)));
+                getCvarStub.withArgs('game_type').returns(new Promise(resolve => resolve(0)));
+                getMapNameStub.returns(new Promise(resolve => resolve('office')));
+                const recordingHelper = new DemoRecordingHelper();
+                subMan.subscribe(recordingHelper);
+                const canHandleSpy = sandbox.spy(recordingHelper, "canHandle");
+                const handleLineSpy = sandbox.spy(recordingHelper, "handleLine");
+                const setPlayerVolumeStub = sandbox.stub(VoicePlayerVolume, "setVoicePlayerVolumeByName");
+                expect(DemoRecordingHelper.synchronouslyCheckIfRecording()).eq(false);
+                await subMan.init();
+                await subMan.begin();
+                expect(canHandleSpy.callCount).eq(2);
+                expect(handleLineSpy.callCount).eq(2);
+                expect(setPlayerVolumeStub.callCount).eq(1);
+                expect(getCvarStub.calledWith('game_mode')).to.eq(true);
+                expect(getCvarStub.calledWith('game_type')).to.eq(true);
+                expect(setCvarStub.callCount).eq(1);
+                expect(DemoRecordingHelper.synchronouslyCheckIfRecording()).eq(false);
+            });
+        });
+
+        describe("Handling naming conflicts + user decisions", function () {
+            it(`All defaults. New 'office-1-8-2021-2-pt3' exists already`, async function () {
+            });
+        });
+    });
+
+    describe("Single function tests", function () {
+        const sampleDemoName = "office-1-8-2021";
+        describe("findLatestDemoWithName", function () {
+            let recordingHelper: DemoRecordingHelper;
+            beforeEach(function () {
+                recordingHelper = new DemoRecordingHelper();
+            });
+            afterEach(function () {
+                mock.restore();
+            });
+            it("can tell when there are no demos", function () {
+                mock({
+                    get [join(config.csgo.csgo_demos_folder)]() {
+                        return {};
                     }
                 });
-                //Put the info about a player being muted into the console
-                s.write(`${DemoRecordingHelper.BeginRecordingCommand}\n`);
+                const result: Pair<number, number> = new DemoRecordingHelper().findLatestDemoWithName(sampleDemoName);
+                const highestDemoNumber = result[0];
+                const highestPartNumber = result[1];
+                expect(highestDemoNumber).to.eq(0);
+                expect(highestPartNumber).to.eq(0);
             });
-            //Uncomment this line to get logger output during this test
-            // LogHelper.configure(config)
-            const recordingHelper = new DemoRecordingHelper();
-            subMan.subscribe(recordingHelper);
-            const canHandleSpy = sandbox.spy(recordingHelper, "canHandle");
-            const handleLineSpy = sandbox.spy(recordingHelper, "handleLine");
-            //We use a stub here instead of a spy because the unmuting action needn't complete since that's not part of the SUT
-            const setPlayerVolumeStub = sandbox.stub(VoicePlayerVolume, "setVoicePlayerVolumeByName");
-            expect(DemoRecordingHelper.synchronouslyCheckIfRecording()).eq(false);
-            await subMan.init();
-            await subMan.begin();
-            expect(canHandleSpy.callCount).eq(23);
-            expect(handleLineSpy.callCount).eq(2);
-            expect(setPlayerVolumeStub.callCount).eq(1);
-            expect(DemoRecordingHelper.synchronouslyCheckIfRecording()).eq(false);
+
+            it("can tell that one demo exists", function () {
+                mock({
+                    get [join(config.csgo.csgo_demos_folder, sampleDemoName + '.dem')]() {
+                        return Buffer.from([8, 6, 7, 5, 3, 0, 9]);
+                    }
+                });
+                const result: Pair<number, number> = new DemoRecordingHelper().findLatestDemoWithName(sampleDemoName);
+                const highestDemoNumber = result[0];
+                const highestPartNumber = result[1];
+                expect(highestDemoNumber).to.eq(1);
+                expect(highestPartNumber).to.eq(1);
+            });
+
+            it("can tell that the latest demo exists and has two parts", function () {
+                mock({
+                    get [join(config.csgo.csgo_demos_folder, `${sampleDemoName}.dem`)]() {
+                        return Buffer.from([8, 6, 7, 5, 3, 0, 9]);
+                    },
+                    get [join(config.csgo.csgo_demos_folder, `${sampleDemoName}-pt2.dem`)]() {
+                        return Buffer.from([8, 6, 7, 5, 3, 0, 9]);
+                    }
+                });
+                const result: Pair<number, number> = new DemoRecordingHelper().findLatestDemoWithName(sampleDemoName);
+                const highestDemoNumber = result[0];
+                const highestPartNumber = result[1];
+                expect(highestDemoNumber).to.eq(1);
+                expect(highestPartNumber).to.eq(2);
+            });
+
+            it("can tell that the latest demo exists and is a sequel", function () {
+                mock({
+                    get [join(config.csgo.csgo_demos_folder, sampleDemoName + '.dem')]() {
+                        return Buffer.from([8, 6, 7, 5, 3, 0, 9]);
+                    },
+                    get [join(config.csgo.csgo_demos_folder, sampleDemoName + '-2.dem')]() {
+                        return Buffer.from([8, 6, 7, 5, 3, 0, 9]);
+                    }
+                });
+                const result: Pair<number, number> = new DemoRecordingHelper().findLatestDemoWithName(sampleDemoName);
+                const highestDemoNumber = result[0];
+                const highestPartNumber = result[1];
+                expect(highestDemoNumber).to.eq(2);
+                expect(highestPartNumber).to.eq(1);
+            });
+
+            it("can tell that the latest demo exists and is a sequel with three parts", function () {
+                mock({
+                    get [join(config.csgo.csgo_demos_folder, sampleDemoName + '.dem')]() {
+                        return Buffer.from([8, 6, 7, 5, 3, 0, 9]);
+                    },
+                    get [join(config.csgo.csgo_demos_folder, sampleDemoName + '-2.dem')]() {
+                        return Buffer.from([8, 6, 7, 5, 3, 0, 9]);
+                    },
+                    get [join(config.csgo.csgo_demos_folder, sampleDemoName + '-2-pt2.dem')]() {
+                        return Buffer.from([8, 6, 7, 5, 3, 0, 9]);
+                    },
+                    get [join(config.csgo.csgo_demos_folder, sampleDemoName + '-2-pt3.dem')]() {
+                        return Buffer.from([8, 6, 7, 5, 3, 0, 9]);
+                    }
+                });
+                const result: Pair<number, number> = new DemoRecordingHelper().findLatestDemoWithName(sampleDemoName);
+                const highestDemoNumber = result[0];
+                const highestPartNumber = result[1];
+                expect(highestDemoNumber).to.eq(2);
+                expect(highestPartNumber).to.eq(3);
+            });
+        });
+
+        describe("mostRecentDemoInfoToString", function () {
+            it("should produce the correct name for a demo that doesn't exist yet (i.e. no naming conflict)", function () {
+                expect(DemoRecordingHelper.mostRecentDemoInfoToString(sampleDemoName, [0, 0])).to.eq(sampleDemoName);
+            });
+            it("should produce the correct name for a demo that exists currently and has no extra parts", function () {
+                expect(DemoRecordingHelper.mostRecentDemoInfoToString(sampleDemoName, [1, 1])).to.eq(sampleDemoName);
+            });
+            it("should produce the correct name for a demo that exists in two parts", function () {
+                expect(DemoRecordingHelper.mostRecentDemoInfoToString(sampleDemoName, [1, 2])).to.eq(sampleDemoName + '-pt2');
+            });
+            it("should produce the correct name for a sequel demo", function () {
+                expect(DemoRecordingHelper.mostRecentDemoInfoToString(sampleDemoName, [2, 1])).to.eq(sampleDemoName + '-2');
+            });
+            it("should produce the correct name for a sequel demo with three parts", function () {
+                expect(DemoRecordingHelper.mostRecentDemoInfoToString(sampleDemoName, [2, 3])).to.eq(sampleDemoName + '-2-pt3');
+            });
         });
     });
 });
